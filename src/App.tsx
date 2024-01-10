061ad71a32df353710fa377fcbabf19c604db858
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-01-10 11:05:55 -0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ToDo from "./components/ToDo";
 
 const App = () => {
   return (
     <>
-      <h1>React TODO Advance using TypeScript</h1>
+      <ToDo />
     </>
   );
 };
