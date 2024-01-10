33427a69e701467316103347a0805d9dc3b66261
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-01-10 13:06:35 -0800

    Intial commit

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const generateID = () => {
+    return Math.floor(Math.random() * 10);
+};
+
+const ToDo = () => {
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState("");
+
+    const handleSubmit = () => {
+        setTodos((todos) => todos.concat({ id: generateID(), text: input }));
+        setInput("");
+    };
+
+    const removeTodo = (id) => setTodos((todo) => todo.filter(t => t.id !== id));
+
+    return (
+        <div className="container">
+            <input 
+                type="text" 
+                placeholder="Type Something..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+
+            <button onClick={handleSubmit}>Add TODO</button>
+
+            <ul className="todos-list">
+                {todos.map((text, id) => {
+                    <li key={id} className="todo">
+                        <span>{text}</span>
+                        <button className="close" onClick={() => removeTodo(id)}>X</button>
+                    </li>
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default ToDo;
