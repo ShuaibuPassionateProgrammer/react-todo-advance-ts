97073dbdd0d2844d33eb33549859757e51799794
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-08-17 17:24:47 -0700

    Improved

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,41 +2,45 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const generateID = () => {
-    return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10);
 };
 
 const ToDo = () => {
-    const [todos, setTodos] = useState([]);
-    const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
-    const handleSubmit = () => {
-        setTodos((todos) => todos.concat({ id: generateID(), text: input }));
-        setInput("");
-    };
+  const handleSubmit = () => {
+    setTodos((todos) => todos.concat({ id: generateID(), text: input }));
+    setInput("");
+  };
 
-    const removeTodo = (id) => setTodos((todo) => todo.filter(t => t.id !== id));
+  const removeTodo = (id) => setTodos((todo) => todo.filter((t) => t.id !== id));
 
-    return (
-        <div className="container">
-            <input 
-                type="text" 
-                placeholder="Type Something..."
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Type Something..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
 
-            <button onClick={handleSubmit}>Add TODO</button>
+      <button onClick={handleSubmit}>Add TODO</button>
 
-            <ul className="todos-list">
-                {todos.map((text, id) => {
-                    <li key={id} className="todo">
-                        <span>{text}</span>
-                        <button className="close" onClick={() => removeTodo(id)}>X</button>
-                    </li>
-                })}
-            </ul>
-        </div>
-    );
+      <ul className="todos-list">
+        {todos.map(({ id, text }) => {
+          return (
+            <li key={id} className="todo">
+              <span>{text}</span>
+              <button className="close" onClick={() => removeTodo(id)}>
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ToDo;
